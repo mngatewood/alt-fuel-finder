123d3fdadc9bccd66ff4989c7f174833d6e4e2b7
Michael Gatewood <michael@mngatewood.com>
2018-12-12 11:04:54 -0700

    Faraday conn working, but needs refactoring, need to get tests to pass

--- app/facades/search_facade.rb
+++ app/facades/search_facade.rb
@@ -1,20 +1,29 @@
 class SearchFacade
 
-  def stations
-    
+  def initialize(location)
+    @location = location
   end
 
-  def get_json(url)
-    response = conn.get(url)
+  def stations
+    @stations ||= get_json[:fuel_stations]
+    binding.pry
+  end
+  
+  # def fetch_stations_response
+  #   get_json("&location=#{@location}")
+  # end
+  
+  def get_json
+    response = conn.get
     JSON.parse(response.body, symbolize_names: true)
   end
 
   def conn
-    Faraday.new(:url => 'https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest') do |faraday|
+    Faraday.new(:url => 'https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json') do |faraday|
       faraday.adapter  Faraday.default_adapter
+      faraday.params["api_key"] = ENV["API_KEY"]
+      faraday.params["location"] = @location
     end
+
   end
-end
-
-
 end
