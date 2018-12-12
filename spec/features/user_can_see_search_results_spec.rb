b27bf7815382e6c12c24884a291c453f1f7c2b79
Michael Gatewood <michael@mngatewood.com>
2018-12-12 09:27:46 -0700

    WIP adds search result tests with some placeholders, will update asserts

---
+++ spec/features/user_can_see_search_results_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+describe 'when I enter a zip and click locate on the home page' do
+
+  before(:each) do
+    visit root_path
+    fill_in "input#q.form-control" with "80203"
+    click_on "Locate"
+  end
+
+  it 'displays 10 closest stations within 6 miles' do
+    within("section.stations") do
+      expect(page).to have_css("li.station", count => 10)
+      expect(page).to have_content("closest station")
+      expect(page).to have_content("tenth station")
+      expect(page).to_not have_content("eleventh station")
+    end
+  end
+
+  it 'sorts stations by distance' do
+    within("station.sections") do
+      # expect first distance to be less than second distance, etc.
+    end
+  end
+
+  it 'only displays electric and propane stations' do
+    within("station.sections") do
+      expect(page).to_not have_content("other fuel type")
+      expect(page).to_not have_content("other fuel type")
+      expect(page).to_not have_content("other fuel type")
+    end
+  end
+
+  it 'displays name, address, fuel types, distance, and access times for each station' do
+    within("li.station#1") do
+      expect(page).to have_content("name")
+      expect(page).to have_content("address")
+      expect(page).to have_content("fuel types")
+      expect(page).to have_content("distance")
+      expect(page).to have_content("access time")
+    end
+  end
+
+end
